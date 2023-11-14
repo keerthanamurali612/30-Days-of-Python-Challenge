abba2cd953a56cf8fd43f1bc1ad1640f48aac052
Keerthana Murali <68218079+keerthanamurali612@users.noreply.github.com>
2023-11-14 20:35:23 +0530

    display monument using if-else

---
+++ Monument of city.py
@@ -0,0 +1,11 @@
+user=input("Enter the city:")
+
+#if user input matches this display the print function
+if user=="Delhi":
+    print("Monument name is :Red Fort")
+elif user=="Agra":
+    print("Monument name is :Taj Mahal")
+elif user=="Jaipur":
+    print("Monument name is :Jal Mahal")
+else:
+    print("Enter the correct name of city")
